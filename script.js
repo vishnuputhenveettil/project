0c670d86c7d718b21a73370992c10eb08c7a1870
vishnuputhenveettil <69865550+vishnuputhenveettil@users.noreply.github.com>
2021-01-19 11:10:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".mca").click(function(){
+  window.location.href = 'mca.html';
+});
+$(".mcom").click(function(){
+  window.location.href = 'mcom.html';
+});
+$(".mba").click(function(){
+  window.location.href = 'mba.html';
+});
